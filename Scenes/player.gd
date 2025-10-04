d1c8942a40701f16f89536e6cafb4643984a16f7
Dany <danixps1@gmail.com>
2025-10-04 10:10:29 -0600

    Animacion y scripts

---
+++ Scenes/player.gd
@@ -0,0 +1,7 @@
+extends CharacterBody2D
+
+const PLAYER_SPEED = 120
+const PLAYER_DIRECCION = 0.0
+
+func _physics_process(delta: float) -> void:
+	
--- Scripts/Constantes.gd
+++
@@ -1,7 +0,0 @@
-extends Node
-#Constantes Globales (Para físicas)
-
-const GRAVITY = 980
-const PLAYER_SPEED = 200
-const JUMP_VELOCITY = -400
-const ENEMY_SPEED = 50
